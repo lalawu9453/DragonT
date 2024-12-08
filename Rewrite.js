2cdc556cae8be3b6437b95fe80f0b72590665e2d
lalawu9453 <lalawu499@gmail.com>
2024-12-09 02:11:00 +0800

    Create Rewrite.js

---
+++ Rewrite.js
@@ -0,0 +1,10 @@
+{
+    "orderAmount" : 648,
+    "cmgeOrderNum" : "96236489AS10000010727A",
+    "gameProps" : "com.lzqy.mlcnnew_100002",
+    "error_description" : "成功",
+    "platformOrderNum" : "96236489AS10000010727A",
+    "codes" : 0,
+    "error_code" : 1000,
+    "appAccountToken" : "00000000-0096-2364-89AB-10000010727A"
+  }
