53112d0c29ebf53d69bde3070756bcac593cc6cb
lalawu9453 <lalawu499@gmail.com>
2024-12-12 09:22:47 +0800

    Update DragonO.js

--- DragonO.js
+++ DragonO.js
@@ -7,10 +7,14 @@ var obj = JSON.parse(body);
 // obj = JSON.parse(body);
 // obj = JSON.parse($response.body);
 obj.data.gameProps = "com.lzqy.mlcnnew_100002";
+obj.data.replace(
+    /("gameProps"\s*:\s*")com\.lzqy\.mlcnnew_100008(")/,
+    '$1com.lzqy.mlcnnew_100002$2'
+  );
 body = JSON.stringify(obj);
 
 $done({body});
 
 
-$notification.post("Storm Sniffer","Network Debug Tool","Dragon Get it!!!")
+$notification.post("Storm Sniffer2","Network Debug Tool2","Dragon Get it!!!")
 
